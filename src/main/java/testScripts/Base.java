b87a2bab549afe0c35000aa7f034a7471b7f0d2d
Anush <anushgemstone@gmail.com>
2025-12-31 11:46:26 +0530

    Push date 31/12/25

--- src/main/java/testScripts/Base.java
+++ src/main/java/testScripts/Base.java
@@ -10,7 +10,7 @@ public class Base {
 	public void browserInit()
 	{
 		driver =new ChromeDriver();
-//		driver.get("https://www.amazon.in/");
+		driver.get("https://selenium.qabible.in/simple-form-demo.php");
 		driver.manage().window().maximize();
 	}
 	
--- src/main/java/testScripts/cssselectorlocator.java
+++ src/main/java/testScripts/cssselectorlocator.java
@@ -4,27 +4,51 @@ import org.openqa.selenium.*;
 
 public class cssselectorlocator extends Base
 {
+	
 	public void tagandid()
 	{
 		WebElement tid= driver.findElement(By.cssSelector("input#single-input-field"));
+		tid.isDisplayed();
+		tid.click();
+		tid.sendKeys("Text1");
 	}
+	
 	public void tagandclass()
 	{
-		
+		WebElement tid= driver.findElement(By.cssSelector("input.form-control"));
+		tid.isDisplayed();
+		tid.click();
+		tid.clear();
+		tid.sendKeys("Text2");
 	}
 	public void tagandattribute()
 	{
-		
+		WebElement tid= driver.findElement(By.cssSelector("input[id='single-input-field']"));
+		tid.isDisplayed();
+		tid.click();
+		tid.clear();
+		tid.sendKeys("Text3");
 	}
+	
 	public void tagandclassattribute()
 	{
-		
+		WebElement tid= driver.findElement(By.cssSelector("input.form-control[id='single-input-field']"));
+		tid.isDisplayed();
+		tid.click();
+		tid.clear();
+		tid.sendKeys("Text4");
 	}
 	
 
 	public static void main(String[] args) 
 	{
-
+		cssselectorlocator css =new cssselectorlocator();
+		css.browserInit();
+		css.tagandid();
+		css.tagandclass();
+		css.tagandattribute();
+		css.tagandclassattribute();
+		css.closeSession();
 	}
 
 }
